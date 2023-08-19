bb373d26581e85fcfa811aadc790e53bfe0f8d60
Nina Krawczak <ninakr06@gmail.com>
2023-08-19 14:57:56 +0200

    Full work with Postgres database. Players have names and surnames, and can be chosen for a match. Cleaner code

--- init.sql
+++ init.sql
@@ -6,10 +6,31 @@ CREATE TYPE results AS ENUM (
     'tie'
 );
 
--- CREATE TABLE
+-- CREATE TABLES
+CREATE TABLE IF NOT EXISTS players (
+    player_id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL
+);
+
+
 --DROP TABLE IF EXISTS games;
 CREATE TABLE IF NOT EXISTS games (
     game_id SERIAL PRIMARY KEY NOT NULL,
     date TIMESTAMP NOT NULL,
-    result results NOT NULL
+    result results NOT NULL,
+    player_id_ONE INT NOT NULL,
+    player_id_TWO INT NOT NULL,
+    CONSTRAINT fk_player_ONE
+        FOREIGN KEY(player_id_ONE)
+	        REFERENCES players(player_id),
+	CONSTRAINT fk_player_TWO
+        FOREIGN KEY(player_id_TWO)
+	        REFERENCES players(player_id)
 );
+
+-- INSERT VALUES
+INSERT INTO players VALUES (default, 'Tomasz', 'Sikora');
+INSERT INTO players VALUES (default, 'Katarzyna', 'Zalesińska');
+INSERT INTO players VALUES (default, 'Natalia', 'Kowal');
+INSERT INTO players VALUES (default, 'Jarosław', 'Nowak');
--- judgeService/judge.py
+++ judgeService/judge.py
@@ -3,36 +3,11 @@ from pydantic import BaseModel
 from typing import Dict
 import requests
 import json
-import datetime
 import psycopg2
 import os
-from dotenv import load_dotenv
-
-
-winsGlobal = { #zapamietywanie wynikow rozgrywek
-    "player1": 0,
-    "player2": 0,
-    "tie": 0
-}
+import random
 
 app = FastAPI()
-
-"""
-    data:
-    {
-        matrix: {[[0, 0, 0],
-                 [0, 0, 0],
-                 [0, 0, 0]]},
-        whoseTurn: "player1"/"player2", #domyslnie 1 zaczyna, moge zmienic ze zaczyna loser
-        wins: {
-                player1: /liczba/,
-                player2: /liczba/,
-                tie: /liczba/
-            }
-        #winner: "None"/"player1"/"player2"
-    }
-
-"""
 
 def dbconnect():
     cnxn = psycopg2.connect(dbname=os.getenv('POSTGRES_DB'),
@@ -46,51 +21,19 @@ def dbconnect():
 class Data(BaseModel):
     input_json: Dict
 
+class Player(BaseModel):
+    name: str
+    surname: str
+
 @app.get("/")
 async def root():
-    print(os.getenv('POSTGRES_DB'))
-    return {"message": "Haii :3"}
+    return {"message": ":3"}
 
 #start rozgrywki
-@app.get("/start")
-def startGame():
-    #czysty słownik na start
-    data = {
-        "input_json": {
-            "matrix":
-                    [
-                        [0, 0, 0],
-                        [0, 0, 0],
-                        [0, 0, 0]
-                    ],
-        "whoseTurn": "player1",
-        "wins": {
-                "player1": 0,
-                "player2": 0,
-                "tie": 0
-            }
-        }
-    }
-
-    #pierwsze przeslanie pustego jsona
-    r = requests.post(url="http://player1:8000/putmark", data=json.dumps(data)) #json.dumps() - wysylasz klase BASEMODEL
-
-    return {"game": "has started"}
-
-@app.get("/stats")
-def gamesStats():
-    #pobieramy ze zmiennej globalnej wyniki
-    #ZMIEN NA BAZE DANYCH
-    return winsGlobal
-
-@app.put("/updateWins")
-def winners(data: Data):
-    def add_record(cursor, cnxn, winner):
-        current_time = datetime.datetime.now()
-        str_date_time = current_time.strftime("%d-%m-%Y %H:%M:%S")
-        query_result = ""
-
-        query = f"INSERT INTO games VALUES (DEFAULT, NOW(), '{winner}') RETURNING game_id;"
+@app.post("/start")
+def startGame(player1: Player, player2: Player):
+    def check_if_exists_DB(cursor, cnxn, player_name, player_surname):
+        query = f"SELECT EXISTS(SELECT 1 FROM players WHERE name='{player_name}' AND surname='{player_surname}')"
         try:
             cursor.execute(query)
         except Exception as e:
@@ -101,26 +44,94 @@ def winners(data: Data):
 
         return query_result
 
-    #sent_data = data.input_json
-    wins = data.input_json["wins"]
-    winner = ""
+    def get_player_id(cursor, cnxn, player_name, player_surname):
+        query = f"SELECT player_id FROM players WHERE name='{player_name}' AND surname='{player_surname}'"
+        try:
+            cursor.execute(query)
+        except Exception as e:
+            query_result = str(e)
+        else:
+            cnxn.commit()
+            query_result = cursor.fetchall()[0]
 
-    global winsGlobal
-    if (wins["player1"] == 1):
-        winsGlobal["player1"] += 1
-        winner = "player1"
-    if (wins["player2"] == 1):
-        winsGlobal["player2"] += 1
-        winner = "player2"
-    if (wins["tie"] == 1):
-        winsGlobal["tie"] += 1
-        winner = "tie"
+        return query_result
 
-    query_result = "uhoh"
 
     try:
         cursor, connection = dbconnect()
-        query_result = add_record(cursor, connection, winner)
+        player1_exist = check_if_exists_DB(cursor, connection, player1.name, player1.surname)
+        player2_exist = check_if_exists_DB(cursor, connection, player2.name, player2.surname)
+    except Exception as e:
+        print(f"Wystąpił błąd: {e}")
+    finally:
+        cursor.close()
+        connection.close()
+
+    if(player1_exist[0] and player2_exist[0]): #jeśli tacy gracze są w DB:
+        try:
+            cursor, connection = dbconnect()
+            player1_id = get_player_id(cursor, connection, player1.name, player1.surname)
+            player2_id = get_player_id(cursor, connection, player2.name, player2.surname)
+        except Exception as e:
+            print(f"Wystąpił błąd: {e}")
+        finally:
+            cursor.close()
+            connection.close()
+
+        if (player1_id != player2_id): #jeśli są to inni gracze
+            data = {
+                "input_json": {
+                    "matrix":
+                        [
+                            [0, 0, 0],
+                            [0, 0, 0],
+                            [0, 0, 0]
+                        ],
+                    "player1_id": player1_id[0],
+                    "player2_id": player2_id[0],
+                    "wins": {
+                        "player1": 0,
+                        "player2": 0,
+                        "tie": 0
+                    }
+                }
+            }
+
+            who_starts = random.randint(0, 1)
+
+            if(who_starts == 0):
+                r = requests.post(url="http://player1:8000/putmark", data=json.dumps(data))
+
+            else:
+                r = requests.post(url="http://player2:8000/putmark", data=json.dumps(data))
+            return {"msg": "rozpoczęto rozgrywke"}
+
+        else:
+            return {"msg": "Gracze muszą być różnymi osobami"}
+
+    else:
+        return {"msg": "Nie ma takich graczy; nie można odegrać rozgrywki"}
+
+
+@app.get("/showPlayers")
+def showPlayers():
+    def players_from_DB(cursor, connection):
+        result_query = []  # zapis wyników
+        query = 'SELECT * FROM players'
+        cursor.execute(query)
+        columns = [column[0] for column in cursor.description]
+        for row in cursor.fetchall():
+            row_dict = {}  # slowniki na rekordy
+            for i, element in enumerate(row):
+                row_dict[
+                    columns[i]] = element
+            result_query.append(row_dict)
+        return result_query
+
+
+    try:
+        cursor, connection = dbconnect()
+        query_result = players_from_DB(cursor, connection)
     except Exception as e:
         print(f"Wystąpił błąd: {e}")
     finally:
@@ -128,8 +139,74 @@ def winners(data: Data):
         cursor.close()
         connection.close()
 
-    """
-    with open('scoresVol/WinHistory.csv', 'a+') as f:
-        f.write(str_date_time + ", " + str(winsGlobal)[1:-1] + ";\n")
-    """
+    return query_result
 
+
+@app.get("/stats")
+def gamesStats():
+    def show_games(cursor, connection):
+        result_query = []
+        query = 'SELECT * FROM games'
+        cursor.execute(query)
+        columns = [column[0] for column in cursor.description]
+        for row in cursor.fetchall():
+            row_dict = {}
+            for i, element in enumerate(row):
+                row_dict[
+                    columns[i]] = element
+            result_query.append(row_dict)
+        return result_query
+
+    try:
+        cursor, connection = dbconnect()
+        query_result = show_games(cursor, connection)
+    except Exception as e:
+        print(f"Wystąpił błąd: {e}")
+    finally:
+        print(query_result)
+        cursor.close()
+        connection.close()
+
+    return query_result
+
+@app.put("/updateWins")
+def winners(data: Data):
+    def add_record(cursor, cnxn, winner, player1_id, player2_id):
+        query_result = ""
+        query = f"INSERT INTO games(game_id, date, result, player_id_one, player_id_two) " \
+                f"VALUES (default, NOW(), '{winner}', {player1_id}, {player2_id});"
+
+        try:
+            cursor.execute(query)
+        except Exception as e:
+            query_result = str(e)
+        else:
+            cnxn.commit()
+            query_result = cursor.fetchall()[0]
+
+        return query_result
+
+    wins = data.input_json["wins"]
+    winner = ""
+    query_result = ""
+
+    if (wins["player1"] == 1):
+        winner = "player1"
+    if (wins["player2"] == 1):
+        winner = "player2"
+    if (wins["tie"] == 1):
+        winner = "tie"
+
+    player1_id = data.input_json["player1_id"]
+    player2_id = data.input_json["player2_id"]
+
+
+    try:
+        cursor, connection = dbconnect()
+        query_result = add_record(cursor, connection, winner, player1_id, player2_id)
+    except Exception as e:
+        print(f"Wystąpił błąd: {e}")
+    finally:
+        print(query_result)
+        cursor.close()
+        connection.close()
